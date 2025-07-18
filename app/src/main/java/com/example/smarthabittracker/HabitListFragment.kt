4a1e38e8f7969a6daa411db4ad3e8fcb36a4f1d8
Smit275-z <w0849357@myscc.ca>
2025-07-17 21:15:29 -0300

    App Navigation Using Fragments implemented

--- app/src/main/java/com/example/smarthabittracker/HabitListFragment.kt
+++ app/src/main/java/com/example/smarthabittracker/HabitListFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 
-class FirstFragment : Fragment() {
+class HabitListFragment : Fragment() {
 
     private val FILE_NAME = "habits.txt"
 
